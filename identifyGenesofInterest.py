3d243e04537ff6a6a5b7b57d80cc3660bf3b2514
sushmitaS16 <sushmita.iitr16@gmail.com>
2021-04-22 01:58:46 +0530

    identify count matrix features among annotation data

---
+++ identifyGenesofInterest.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+'''
+Script to identify set of genes/transcripts from a countmatrix in the annotation file
+@sushmitaS16
+'''
+import sys
+import argparse
+import pandas as pd
+import numpy as np 
+
+
+def getFeatureNames(annotation_data):
+	''' function to add an additional column in the annotation table with the feature name (obtained from the 'attribute' column) '''
+
+	# from the annotation df take the 'attribute' col as a list
+	attr = annotation_data.attribute
+	attr_len = len(attr)
+	feature_names = []
+
+	for i in range(0,attr_len):
+		attr_info = attr[i].split(";") # split on semicolon to form a new list
+		# obtain the 'gene_name' from this newly formed list (name is usually the 2nd or 3rd element in this list; check to make sure)
+		gene_with_name = attr_info[1]	# or attr_info[2]; attr_info[0] is almost always the 'ID'
+		gene = gene_with_name.split("=")	# or gene_with_name.split(" ")
+		feature_names.append(gene[1])
+
+	annotation_data['feature_names'] = feature_names
+
+	return annotation_data
+
+
+
+def savefile(annotation_data, fwd_annotation_data, rev_annotation_data, outfile):
+	''' function to save relevant files '''
+	files_toSave = input("Whether forward/reverse strand based seperation required (y/n)? :  ")
+
+	if files_toSave == 'y' or files_toSave == 'Y':
+		fwd_annotation_data.to_csv(outfile + 'forward_stranded' + '.csv', sep='\t', index=False)
+		rev_annotation_data.to_csv(outfile + 'reverse_stranded' + '.csv', sep='\t', index=False)
+		print("**** 2 files successfully extracted! ****")
+
+	elif files_toSave == 'n' or files_toSave == 'N':
+		annotation_data.to_csv(outfile + '.csv', sep='\t', index=False)
+		print("**** 1 file successfully extracted! ****")
+
+
+def main():
+
+	desc = "Script to identify set of genes/transcripts from a list in the annotation file"
+
+	#initialize parser
+	parser = argparse.ArgumentParser(description=desc)
+	parser.add_argument("-a", "--annotation", help="Input gene/transcript annotation file (in dataframe format) (.csv)")
+	parser.add_argument("-f", "--features", help="Input countmatrix of genes/transcripts to be identified (.txt/.csv)")
+	parser.add_argument("-o", "--outfile", help="Output file with genes/transcripts of interest")
+
+	# read arguments from command line
+	args = parser.parse_args()
+
+	# defining the inputs
+	anno_df = args.annotation
+	feats_df = args.features
+	out_df = args.outfile
+
+	modified_anno_df = getFeatureNames(anno_df)
+	feats_list = feat_df.iloc[:,0]
+
+	# extract out rows specific rows
+	new_anno_df = modified_anno_df[modified_anno_df.feature_name.isin(feats_list)]
+
+
+	# separate the new dataframe based on sequence strandedness
+	forward_df = new_anno_df[new_anno_df.strand == '+']
+	reverse_df = new_anno_df[new_anno_df.strand == '-']
+
+	savefile(new_anno_df, forward_df, reverse_df, out_df)
+
+	return 0
+
+
+if __name__ == "__main__":
+	main()
